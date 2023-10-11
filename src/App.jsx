c097039641c24b5f544c8793043f2bb890254375
abrahamsantos-developer <abesantostorres@gmail.com>
2023-10-11 14:17:33 -0600

    created todo and info components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import "./style.css";
 
+import "./style.css";
+import { useState, useEffect } from "react";
+import { Form } from "./Form";
+import { Info} from "./Info";
 
 function App() {
   const [count, setCount] = useState(0)
